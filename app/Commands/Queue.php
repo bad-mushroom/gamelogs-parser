2789e254613c5bae5935860dafe6e7203fd58385
Chris Sprague <chris@chaoscontrol.org>
2022-09-01 08:02:22 -0400

    Add command to queue log files

--- app/Commands/Queue.php
+++ app/Commands/Queue.php
@@ -2,21 +2,32 @@
 
 namespace App\Commands;
 
+use App\Exceptions\DuplicateGamelogException;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use LaravelZero\Framework\Commands\Command;
+use SplFileInfo;
 
+/**
+ * Queue command.
+ */
 class Queue extends Command
 {
+    public const STATUS_INCOMING = 1;
+    public const STATUS_QUEUED = 2;
+    public const STATUS_OUTGOING = 3;
+
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'queue';
+    protected $signature = 'logs:queue';
 
     /**
      * The description of the command.
@@ -36,7 +47,11 @@ class Queue extends Command
     ];
 
     /**
-     * Execute the console command.
+     * Queue log files for parsing.
+     *
+     * Scans "incoming" log directory for files that have not already been parsed
+     * then moves them to "queued". Duplicate files are ignored and will need
+     * to be removed manually.
      *
      * @return mixed
      */
@@ -44,20 +59,27 @@ class Queue extends Command
     {
         $files = File::allFiles(storage_path('gamelogs') . DIRECTORY_SEPARATOR . env('DIR_GAMELOGS_INCOMING'));
 
-        foreach ($files as $file) {
+        $this->info(count($files) . ' Game ' . Str::plural('log', count($files)) .' to Parse');
 
+        foreach ($files as $file) {
             if ($file->isReadable() && !in_array($file->getFilename(), $this->ignoredFiles)) {
                 $hash = md5_file($file);
+
                 try {
-                    // Create record
+                    $this->createGamelogFromFile($file, $hash);
+                    $this->info($file->getFilename() . ' has been queued.');
                 } catch (Exception $e) {
-                    Log::error($e->getMessage());
+                    $this->error($e->getMessage());
+
+                    continue;
                 }
 
-                Storage::disk('gamelogs')->move(env('DIR_GAMELOGS_INCOMING') .'/'. $file->getFilename(), env('DIR_GAMELOGS_QUEUED') . DIRECTORY_SEPARATOR . $hash . '.log');
+                $source = env('DIR_GAMELOGS_INCOMING') . DIRECTORY_SEPARATOR . $file->getFilename();
+                $destination = env('DIR_GAMELOGS_QUEUED') . DIRECTORY_SEPARATOR . $hash . '.log';
+
+                Storage::disk('gamelogs')->move($source, $destination);
             }
         }
-
     }
 
     /**
@@ -69,5 +91,32 @@ class Queue extends Command
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
+    }
+
+    /**
+     * Create `gamelogs` record in database.
+     *
+     * @param SplFileInfo $file
+     * @param string $hash
+     * @return boolean
+     * @throws DuplicateGamelogException
+     */
+    protected function createGamelogFromFile(SplFileInfo $file, string $hash): bool
+    {
+        $existing = DB::scalar('select count(id) from gamelogs where hash = ?', [$hash]);
+
+        if ($existing) {
+            throw new DuplicateGamelogException($file->getFilename() . ' has alrady been parsed. Skipping.');
+        }
+
+        return DB::table('gamelogs')
+            ->insert([
+                'id'                => Str::uuid(),
+                'hash'              => $hash,
+                'original_filename' => $file->getFilename(),
+                'status'            => self::STATUS_INCOMING,
+                'created_at'        => Carbon::now(),
+                'updated_at'        => Carbon::now(),
+            ]);
     }
 }
---
+++ database/migrations/2022_08_31_133431_create_gamelogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gamelogs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('hash', 32);
+            $table->string('original_filename');
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gamelogs');
+    }
+};
